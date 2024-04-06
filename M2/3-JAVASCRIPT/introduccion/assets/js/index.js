0d18a30018375726747020eebaac681a26a22a85
tablonimus <lautamartin1995@gmail.com>
2024-04-05 22:02:01 -0300

    new commity

---
+++ M2/3-JAVASCRIPT/introduccion/assets/js/index.js
@@ -0,0 +1,16 @@
+/* COMENTAR CÓDIGO  */
+// COMENTAR CÓDIGO
+
+/* prompt() => Le permitimos al usuario ingresar una cadena de caracteres(string) */
+/* let nombre = prompt("Escribe tu contraseña aqui!") */
+
+/* alert() => muestra una alerta del navegador */
+// alert("Hola " + nombre + " te ganaste $1.000.000 de pesos");
+
+/* console.log() => me muestra un mensaje en consola */
+console.log("-------------------------------");
+console.log("Te estamos hackeando");
+
+console.log("--------------:)-----------------");
+console.log(7 + 10);
+console.log("hola" + "chau");
