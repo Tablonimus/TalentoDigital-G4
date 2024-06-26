4d2ea3b27618f72e0a40e0a6e06d518c216bf19b
tablonimus <lautamartin1995@gmail.com>
2024-06-25 22:29:19 -0300

    éndientes

--- M6/Unidad1/Dia2/data-example/src/App.vue
+++ M6/Unidad1/Dia2/data-example/src/App.vue
@@ -18,7 +18,7 @@ export default {
   data() {
     return {
       mainTitle: "Mi data.",
-      description: "Mi descricion es cortita.",
+      description: "Mi descripcion es cortita.",
       myName:"Lautaro"
     };
   },
