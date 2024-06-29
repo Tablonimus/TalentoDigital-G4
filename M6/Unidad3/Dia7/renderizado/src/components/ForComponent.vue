e35bdd0291ff96e110dc2319b23d2e0ab044653a
tablonimus <lautamartin1995@gmail.com>
2024-06-28 21:46:28 -0300

    new chjaracter

--- M6/Unidad3/Dia7/renderizado/src/components/ForComponent.vue
+++ M6/Unidad3/Dia7/renderizado/src/components/ForComponent.vue
@@ -8,7 +8,6 @@
     </ul>
 
     <ul>
-      
       <li :key="alumno.rut" v-for="alumno in alumnos">
         <img :src="alumno.img" alt="perfil alumno" />
         <p>
@@ -41,6 +40,68 @@ export default {
           nombre: "Roimer",
           apellido: "Rojas",
           img: "https://indierocks.sfo3.cdn.digitaloceanspaces.com/wp-content/uploads/bfi_thumb/nicolas-jaar-v-5hnqf5in97x879z0gqtimm90y89z2by0nt4153ir9td4ji0e7xhg9rblcljq4g.jpg",
+        },
+      ],
+      personajes: [
+        {
+          id: 1,
+          name: "Rick Sanchez",
+          status: "Alive",
+          species: "Human",
+          type: "",
+          gender: "Male",
+          origin: {
+            name: "Earth (C-137)",
+            url: "https://rickandmortyapi.com/api/location/1",
+          },
+          location: {
+            name: "Citadel of Ricks",
+            url: "https://rickandmortyapi.com/api/location/3",
+          },
+          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+
+          url: "https://rickandmortyapi.com/api/character/1",
+          created: "2017-11-04T18:48:46.250Z",
+        },
+        {
+          id: 2,
+          name: "Morty Smith",
+          status: "Alive",
+          species: "Human",
+          type: "",
+          gender: "Male",
+          origin: {
+            name: "unknown",
+            url: "",
+          },
+          location: {
+            name: "Citadel of Ricks",
+            url: "https://rickandmortyapi.com/api/location/3",
+          },
+          image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+
+          url: "https://rickandmortyapi.com/api/character/2",
+          created: "2017-11-04T18:50:21.651Z",
+        },
+        {
+          id: 3,
+          name: "Summer Smith",
+          status: "Alive",
+          species: "Human",
+          type: "",
+          gender: "Female",
+          origin: {
+            name: "Earth (Replacement Dimension)",
+            url: "https://rickandmortyapi.com/api/location/20",
+          },
+          location: {
+            name: "Earth (Replacement Dimension)",
+            url: "https://rickandmortyapi.com/api/location/20",
+          },
+          image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+
+          url: "https://rickandmortyapi.com/api/character/3",
+          created: "2017-11-04T19:09:56.428Z",
         },
       ],
     };
