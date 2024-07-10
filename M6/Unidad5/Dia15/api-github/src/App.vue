1c1ef3e80b7f249a5ca5d506ef77f74da1d47932
tablonimus <lautamartin1995@gmail.com>
2024-07-10 20:59:36 -0300

    api guithu

---
+++ M6/Unidad5/Dia15/api-github/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <p>{{ user.name }}</p>
+    <p>{{ user.bio }}</p>
+    <a :href="user.html_url" target="_blank">Visitar la página</a>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      user: {},
+    };
+  },
+  async mounted() {
+    try {
+      const { data } = await axios.get("https://api.github.com/users/ingridoa");
+      this.user = data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+</script>
+
+<style></style>
